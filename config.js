1db0eaebfc7563d510c9bb345308647f70ee88b3
nhannguyen02122000 <nhan.nguyen02122000@hcmut.edu.vn>
2021-06-05 21:40:52 +0700

    Change

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    introTitle: 'Hey Việt Anh!',
+    introDesc: `Cậu biết gì không
+    Tôi là một khách hàng khó tính đấy
+    Biết sao không?
+    Vì toi yeu cau rat nhieu :>`,
+    btnIntro: 'Hị Hị',
+    title: 'Chán quá nên làm cái web này, cậu nói đi...',
+    desc: 'Cậu thích toi chứ ;)))',
+    btnYes: 'Cóooo <33',
+    btnNo: 'Không, chia buồn :3',
+    question:'Một tí lý do nào :>',
+    btnReply: 'Gửi cho người có aura thánh thiện',
+    reply: 'Lò Vé <3',
+    mess: 'Tôi biết mà :> :> :>',
+    messDesc: 'Đừng có mà bơ toi nữa :(( chán quớ',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://m.me/tannyan.nguyen' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
